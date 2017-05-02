780b93a62f25363778b77ea0c9ca599f4300a950
kfazz <kfazz01@gmail.com>
2017-05-02 12:47:34 -0400

    revert some xorg changes

--- packages/x11/lib/libXfont2/package.mk
+++ packages/x11/lib/libXfont2/package.mk
@@ -16,21 +16,19 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="libXfont"
-PKG_VERSION="1.5.1"
-PKG_REV="1"
+PKG_NAME="libXfont2"
+PKG_VERSION="2.0.1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain util-macros fontcacheproto fontsproto xtrans freetype libfontenc"
-PKG_PRIORITY="optional"
 PKG_SECTION="x11/lib"
 PKG_SHORTDESC="libxfont: X font Library"
 PKG_LONGDESC="X font Library"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
                            --disable-ipv6 \
